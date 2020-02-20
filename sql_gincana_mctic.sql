62cf0510e4342d3139f20e691689f5c999c9d646
Leandro Chaves <leandrochl@outlook.com>
2020-02-20 09:22:30 -0300

    SQL atualizado

--- sql_gincana_mctic.sql
+++ sql_gincana_mctic.sql
@@ -1,125 +1,65 @@
--- phpMyAdmin SQL Dump
--- version 4.7.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1:3306
--- Generation Time: 13-Fev-2020 às 10:54
--- Versão do servidor: 5.7.19
--- PHP Version: 5.6.31
+DROP TABLE IF EXISTS `usuario`;
+CREATE TABLE IF NOT EXISTS `usuario` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `codigo` smallint(6) NULL,
+  `nome` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `nascimento` datetime DEFAULT NULL,
+  `rede_social` varchar(50) DEFAULT NULL,
+  `tipo_rede_social` varchar(50) DEFAULT NULL,
+  `pontos` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+DROP TABLE IF EXISTS `tipo_atividade`;
+CREATE TABLE IF NOT EXISTS `tipo_atividade` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `descricao` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ;
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `gincana_mctic`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `alternativas_quiz`
---
+DROP TABLE IF EXISTS `atividade`;
+CREATE TABLE IF NOT EXISTS `atividade` (
+  `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(20) DEFAULT NULL,
+  `id_tipo_atividade` smallint(1) DEFAULT NULL,
+  `descricao` varchar(500) DEFAULT NULL,
+  `pontos` smallint(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY `FK_id_tipo_atividade_tb_atividade` (`id_tipo_atividade`) REFERENCES tipo_atividade (id)
+) ;
 
 DROP TABLE IF EXISTS `alternativas_quiz`;
 CREATE TABLE IF NOT EXISTS `alternativas_quiz` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `id_quiz` smallint(6) DEFAULT NULL,
   `descricao` varchar(100) DEFAULT NULL,
-  `resposta_certa` tinyint(1) DEFAULT NULL,
+  `resposta_certa` char(1) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_id_quiz_tb_alternativas_quiz` (`id_quiz`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `atividade_qr`
---
-
-DROP TABLE IF EXISTS `atividade_qr`;
-CREATE TABLE IF NOT EXISTS `atividade_qr` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(500) DEFAULT NULL,
-  `pontuacao` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `atividade_quiz`
---
-
-DROP TABLE IF EXISTS `atividade_quiz`;
-CREATE TABLE IF NOT EXISTS `atividade_quiz` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(500) DEFAULT NULL,
-  `pontuacao` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `pai_filho`
---
-
-DROP TABLE IF EXISTS `pai_filho`;
-CREATE TABLE IF NOT EXISTS `pai_filho` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `id_pai` smallint(6) DEFAULT NULL,
-  `id_filho` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_id_pai_tb_pai_filho` (`id_pai`),
-  KEY `FK_id_filho_tb_pai_filho` (`id_filho`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `premio`
---
+  FOREIGN KEY `FK_id_quiz_tb_alternativas_quiz` (`id_quiz`) REFERENCES atividade (id)
+) ;
 
 DROP TABLE IF EXISTS `premio`;
 CREATE TABLE IF NOT EXISTS `premio` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(20) DEFAULT NULL,
   `descricao` varchar(100) DEFAULT NULL,
   `estoque` smallint(6) DEFAULT NULL,
   `pontos` smallint(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `realiza_atividade`
---
+) ;
 
 DROP TABLE IF EXISTS `realiza_atividade`;
 CREATE TABLE IF NOT EXISTS `realiza_atividade` (
   `id` smallint(6) NOT NULL AUTO_INCREMENT,
   `id_usuario` smallint(6) DEFAULT NULL,
-  `id_tipo_atividade` smallint(6) DEFAULT NULL,
+  `id_atividade` smallint(6) DEFAULT NULL,
   `hora_inicio` datetime DEFAULT NULL,
   `hora_fim` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `FK_id_usuario_tb_realiza_atividade` (`id_usuario`),
-  KEY `FK_id_tipo_atividade_tb_realiza_atividade` (`id_tipo_atividade`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `resgata_premio`
---
+  FOREIGN KEY `FK_id_usuario_tb_realiza_atividade` (`id_usuario`) REFERENCES usuario (id)
+  
+) ;
 
 DROP TABLE IF EXISTS `resgata_premio`;
 CREATE TABLE IF NOT EXISTS `resgata_premio` (
@@ -128,82 +68,11 @@ CREATE TABLE IF NOT EXISTS `resgata_premio` (
   `id_premio` smallint(6) DEFAULT NULL,
   `hora_resgate` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ;
 
---
--- Acionadores `resgata_premio`
---
-DROP TRIGGER IF EXISTS `atualiza_estoque`;
-DELIMITER $$
-CREATE TRIGGER `atualiza_estoque` AFTER INSERT ON `resgata_premio` FOR EACH ROW BEGIN
-   UPDATE premio
-   SET estoque = estoque-1
-   WHERE premio.id = resgata_premio.id;
-END
-$$
-DELIMITER ;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `tipo_atividade`
---
 
-DROP TABLE IF EXISTS `tipo_atividade`;
-CREATE TABLE IF NOT EXISTS `tipo_atividade` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
 
---
--- Estrutura da tabela `tipo_cadastro`
---
 
-DROP TABLE IF EXISTS `tipo_cadastro`;
-CREATE TABLE IF NOT EXISTS `tipo_cadastro` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tipo_usuario`
---
-
-DROP TABLE IF EXISTS `tipo_usuario`;
-CREATE TABLE IF NOT EXISTS `tipo_usuario` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `descricao` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `usuario`
---
-
-DROP TABLE IF EXISTS `usuario`;
-CREATE TABLE IF NOT EXISTS `usuario` (
-  `id` smallint(6) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) DEFAULT NULL,
-  `login` varchar(30) DEFAULT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `senha` varchar(30) DEFAULT NULL,
-  `id_tipo_cadastro` smallint(6) DEFAULT NULL,
-  `id_tipo_usuario` smallint(6) DEFAULT NULL,
-  `pontos` smallint(6) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `FK_id_tipo_cadastro_tb_pai_filho` (`id_tipo_cadastro`),
-  KEY `FK_id_tipo_usuario_tb_pai_filho` (`id_tipo_usuario`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- sql_script.sql
+++ sql_script.sql
@@ -1,1 +1,38 @@
-INSERT INTO usuario (nome,login,email,senha,id_tipo_cadastro,id_tipo_usuario,pontos) values ("max","max","max@gmail.com","123",1,1,5);
+INSERT INTO usuario (nome,email,nascimento,rede_social,tipo_rede_social,pontos) values ("Maxwell Borges","max@gmail.com","1994-02-25","@maxwell.borges","twitter",15);
+INSERT INTO usuario (nome,email,nascimento,rede_social,tipo_rede_social,pontos) values ("Leandro Chaves","leandro@gmail.com","1994-01-21","@leandrochl","instagram",5);
+
+INSERT INTO premio (codigo,descricao,estoque,pontos) VALUES ('MCTIC0002', 'Caderno', 20, 15);
+INSERT INTO premio (codigo,descricao,estoque,pontos) VALUES ('MCTIC0003', 'Lápis', 100, 5);
+INSERT INTO premio (codigo,descricao,estoque,pontos) VALUES ('MCTIC0004', 'Bolsa', 5, 50);
+INSERT INTO premio (codigo,descricao,estoque,pontos) VALUES ('MCTIC0005', 'Celular', 5, 100);
+
+INSERT INTO tipo_atividade (descricao) VALUES ('Atividade física');
+INSERT INTO tipo_atividade (descricao) VALUES ('Quiz');
+
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0006', 1,'Subir na árvore', 15);
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0007', 1,'Cantar o hino', 25);
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0008', 1,'Pular 3x', 10);
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0009', 1,'Tirar foto', 30);
+
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0010', 2,'Quantas horas demora?', 15);
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0011', 2,'Quantos dias demora?', 10);
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0012', 2,'Qual o nome?', 5);
+INSERT INTO atividade (codigo,id_tipo_atividade, descricao,pontos) VALUES ('MCTIC0013', 2,'Qual a idade?', 50);
+
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (5, '5h', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (5, '6h', 'S');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (5, '12h', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (6, '5 dias', 'S');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (6, '6 dias', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (6, '7 dias', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (7, 'Jorge', 'S');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (7, 'Carlos', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (7, 'Maurício', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (8, '13 anos', 'S');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (8, '14 anos', 'N');
+INSERT INTO alternativas_quiz (id_quiz,descricao,resposta_certa) VALUES (8, '15 anos', 'N');
+
+
+
+
+
