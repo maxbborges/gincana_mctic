7c2cc783d503ad0b97c4245c48f3d0ac9b1322db
Maxwell <max15borges@gmail.com>
2020-03-04 22:43:22 -0300

    Inserido footer com click

--- gincana/www/js/footer.js
+++ gincana/www/js/footer.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
     
-    $("#footer").load("footer.html"); 
+    $("#footer").load("footer.html", function(){
+        $("#footer").click(function() {
+            if ($(this).css('height')=='91px'){
+                $(this).css('height', '400px');
+            } else {
+                $(this).css('height', '91px');
+            }
+        });
+    }); 
 
 });
