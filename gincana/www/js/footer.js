d28e462bd7b5cc04f71758b334b9cffc31b94ac4
Leandro Chaves <leandrochl@outlook.com>
2020-03-17 09:29:18 -0300

    Ajuste na setinha footer

--- gincana/www/js/footer.js
+++ gincana/www/js/footer.js
@@ -2,12 +2,22 @@ $( document ).ready(function() {
     
     $("#footer").load("footer.html", function(){
         $("#footer").click(function() {
-            if ($(this).css('height')=='70px'){
+            if ($(this).css('height') == '70px'){
                 $(this).css('height', '294px');
-                $(".patrocinadores").css('display','block');
+                $(".patrocinadores").show();
             } else {
                 $(this).css('height', '70px');
-                $(".patrocinadores").css('display','none');
+                $(".patrocinadores").hide();
+            }
+
+            if($("#img_arrow").hasClass("arrow_up")){
+                $("#img_arrow").attr("src","../img/arrow_down.png");
+                $("#img_arrow").removeClass("arrow_up");
+                $("#img_arrow").addClass("arrow_down");
+            } else {
+                $("#img_arrow").attr("src","../img/arrow_up.png");
+                $("#img_arrow").addClass("arrow_up");
+                $("#img_arrow").removeClass("arrow_down");
             }
         });
     }); 
