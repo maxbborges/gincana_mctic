2b7fbc159c5256f923dec455993de89f5995ceba
Leandro Chaves <leandrochl@outlook.com>
2020-04-13 12:57:54 -0300

    Alterando Plugin de leitura QR

--- gincana/www/js/ler_qr.js
+++ gincana/www/js/ler_qr.js
@@ -1,80 +1,80 @@
-// var atividades = JSON.parse(window.localStorage.getItem('atividades'));
+var atividades = JSON.parse(window.localStorage.getItem('atividades'));
 
 // alert(navigator.platform);
 
-// let scanner = new Instascan.Scanner({
-//         video: document.getElementById('preview')
-// });
-
-// scanner.addListener('scan', function(content) {
-//     verificaQr(content);    
-//     // window.open(content, "_blank");
-// });
-
-
-window.addEventListener("load", function () {
-    setTimeout(() => { 
-        QRScanner.prepare(onDone); 
-    }, 1000);
+let scanner = new Instascan.Scanner({
+        video: document.getElementById('preview')
 });
 
-function onDone(err, status) {
-    if (err) {
-        alert("Não foi possível acessar a câmera do dispositivo");
-        $("#img_load_qr").hide();
-        $("#div_qr_screen").hide();
-        $("#div_qr_manual").show();
-        $("#div_btn_voltar_qr").hide();
-        $("#div_btn_qr_manual").show();
-        $("#span_title_qr").html("Digite o código do QR code");          
-    }
-    if (status.authorized) {
-        $("#img_load_qr").hide();
-        $("#div_qr_screen").show();
-        $("#div_btn_voltar_qr").show();
-        $("#div_btn_qr_manual").hide();
-        $("#span_title_qr").html("Escaneie o código do QR code");
+scanner.addListener('scan', function(content) {
+    verificaQr(content);    
+    window.open(content, "_blank");
+});
 
-        var elements = document.getElementsByClassName('blink')
-        for (var i = 0; i < elements.length; i++) {
-            elements[i].style.display = 'none';
-        }
-        window.QRScanner.show(function (status) {
-            QRScanner.scan(displayContents);
-        });
-    } else if (status.denied) {
-        alert("Não foi possível acessar a câmera do dispositivo");
-        $("#img_load_qr").hide();
-        $("#div_qr_screen").hide();
-        $("#div_qr_manual").show();
-        $("#div_btn_voltar_qr").hide();
-        $("#div_btn_qr_manual").show();
-        $("#span_title_qr").html("Digite o código do QR code");
-    } else {
-        alert("Não foi possível acessar a câmera do dispositivo");
-        $("#img_load_qr").hide();
-        $("#div_qr_screen").hide();
-        $("#div_qr_manual").show();
-        $("#div_btn_voltar_qr").hide();
-        $("#div_btn_qr_manual").show();
-        $("#span_title_qr").html("Digite o código do QR code");
-    }
-}
-function displayContents(err, text) {
-    if (err) {
-        // an error occurred, or the scan was canceled (error code `6`)
-        alert("Ocorreu um erro!");
-        $("#img_load_qr").hide();
-        $("#div_qr_screen").hide();
-        $("#div_qr_manual").show();
-        $("#div_btn_voltar_qr").hide();
-        $("#div_btn_qr_manual").show();
-        $("#span_title_qr").html("Digite o código do QR code");
-    } else {
-        // The scan completed, display the contents of the QR code:
-        verificaQr(text.result);
-    }
-}
+
+// window.addEventListener("load", function () {
+//     setTimeout(() => { 
+//         QRScanner.prepare(onDone); 
+//     }, 1000);
+// });
+
+// function onDone(err, status) {
+//     if (err) {
+//         alert("Não foi possível acessar a câmera do dispositivo");
+//         $("#img_load_qr").hide();
+//         $("#div_qr_screen").hide();
+//         $("#div_qr_manual").show();
+//         $("#div_btn_voltar_qr").hide();
+//         $("#div_btn_qr_manual").show();
+//         $("#span_title_qr").html("Digite o código do QR code");          
+//     }
+//     if (status.authorized) {
+//         $("#img_load_qr").hide();
+//         $("#div_qr_screen").show();
+//         $("#div_btn_voltar_qr").show();
+//         $("#div_btn_qr_manual").hide();
+//         $("#span_title_qr").html("Escaneie o código do QR code");
+
+//         var elements = document.getElementsByClassName('blink')
+//         for (var i = 0; i < elements.length; i++) {
+//             elements[i].style.display = 'none';
+//         }
+//         window.QRScanner.show(function (status) {
+//             QRScanner.scan(displayContents);
+//         });
+//     } else if (status.denied) {
+//         alert("Não foi possível acessar a câmera do dispositivo");
+//         $("#img_load_qr").hide();
+//         $("#div_qr_screen").hide();
+//         $("#div_qr_manual").show();
+//         $("#div_btn_voltar_qr").hide();
+//         $("#div_btn_qr_manual").show();
+//         $("#span_title_qr").html("Digite o código do QR code");
+//     } else {
+//         alert("Não foi possível acessar a câmera do dispositivo");
+//         $("#img_load_qr").hide();
+//         $("#div_qr_screen").hide();
+//         $("#div_qr_manual").show();
+//         $("#div_btn_voltar_qr").hide();
+//         $("#div_btn_qr_manual").show();
+//         $("#span_title_qr").html("Digite o código do QR code");
+//     }
+// }
+// function displayContents(err, text) {
+//     if (err) {
+//         // an error occurred, or the scan was canceled (error code `6`)
+//         alert("Ocorreu um erro!");
+//         $("#img_load_qr").hide();
+//         $("#div_qr_screen").hide();
+//         $("#div_qr_manual").show();
+//         $("#div_btn_voltar_qr").hide();
+//         $("#div_btn_qr_manual").show();
+//         $("#span_title_qr").html("Digite o código do QR code");
+//     } else {
+//         // The scan completed, display the contents of the QR code:
+//         verificaQr(text.result);
+//     }
+// }
 
 
 
@@ -83,37 +83,37 @@ $("#btn_enviar_qr").click(function () {
     verificaQr(text);
 });
 
-// Instascan.Camera.getCameras().then(cameras => {
-//     console.log(cameras);
-//     if (cameras.length > 0) {
-//         $("#img_load_qr").hide();
-//         $("#div_qr_screen").show();
-//         $("#div_btn_voltar_qr").show();
-//         $("#div_btn_qr_manual").hide();
-//         $("#span_title_qr").html("Escaneie o código do QR code");
-//         if (cameras.length == 1) {
-//             scanner.start(cameras[0]);
-//         } else {
-//             scanner.start(cameras[(cameras.length) - 1]);
-//         }
-//     } else {
-//         alert("Não foi possível acessar a câmera do dispositivo");
-//         $("#img_load_qr").hide();
-//         $("#div_qr_screen").hide();
-//         $("#div_qr_manual").show();
-//         $("#div_btn_voltar_qr").show();
-//         $("#div_btn_qr_manual").hide();
-//         $("#span_title_qr").html("Digite o código do QR code");
-//     }
-// }).catch(function (e) {
-//     alert("Não foi possível acessar a câmera do dispositivo");
-//     $("#img_load_qr").hide();
-//     $("#div_qr_screen").hide();
-//     $("#div_qr_manual").show();
-//     $("#div_btn_voltar_qr").hide();
-//     $("#div_btn_qr_manual").show();
-//     $("#span_title_qr").html("Digite o código do QR code");
-// });
+Instascan.Camera.getCameras().then(cameras => {
+    console.log(cameras);
+    if (cameras.length > 0) {
+        $("#img_load_qr").hide();
+        $("#div_qr_screen").show();
+        $("#div_btn_voltar_qr").show();
+        $("#div_btn_qr_manual").hide();
+        $("#span_title_qr").html("Escaneie o código do QR code");
+        if (cameras.length == 1) {
+            scanner.start(cameras[0]);
+        } else {
+            scanner.start(cameras[(cameras.length) - 1]);
+        }
+    } else {
+        alert("Não foi possível acessar a câmera do dispositivo1");
+        $("#img_load_qr").hide();
+        $("#div_qr_screen").hide();
+        $("#div_qr_manual").show();
+        $("#div_btn_voltar_qr").show();
+        $("#div_btn_qr_manual").hide();
+        $("#span_title_qr").html("Digite o código do QR code");
+    }
+}).catch(function (e) {
+    alert("Não foi possível acessar a câmera do dispositivo2");
+    $("#img_load_qr").hide();
+    $("#div_qr_screen").hide();
+    $("#div_qr_manual").show();
+    $("#div_btn_voltar_qr").hide();
+    $("#div_btn_qr_manual").show();
+    $("#span_title_qr").html("Digite o código do QR code");
+});
 
 $(".btn_voltar_ler_qr").click(function () {
     window.location.href = '../paginas/home.html';
